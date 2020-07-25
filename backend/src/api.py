cafb5fc3198f935f18b578065df55701c2908765
Abdulmohsen Alkhamis <Abdulmohsen-alkhamis@hotmail.com>
2020-07-25 23:04:58 +0300

    Backend Configuration
    Configuring the endpoints in the backend

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -27,6 +27,21 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks')
+def get_drinks():
+    try:
+        drinks = Drink.query.all()
+
+        drinks_short = [drink.short() for drink in drinks]
+
+        print(drinks_short)
+
+        return jsonify({
+            'success': True,
+            'drinks': drinks_short
+        })
+    except:
+        abort(404)
 
 
 '''
@@ -38,6 +53,22 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drink_detail(jwt):
+    try:
+        drinks = Drink.query.all()
+
+        drinks_long = [drink.long() for drink in drinks]
+
+        return jsonify({
+            'success': True,
+            'drinks': drinks_long
+        })
+    except:
+        abort(404)
+
+
 
 '''
 @TODO implement endpoint
@@ -48,6 +79,32 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+@app.route('/drinks', methods=["POST"])
+@requires_auth('post:drinks')
+def create_drink(jwt):
+    body = request.get_json()
+
+    #title = 'testTitle'
+    #recipe = 'testRecipe'
+
+    title = body.get('title')
+    recipe = body.get('recipe')
+
+    if not title or not recipe:
+        abort(422)
+
+    try:
+        drink = Drink(title=title, recipe=json.dumps(recipe))
+        drink.insert()
+
+        return jsonify({
+            'success': True,
+            'drinks': drink.long()
+        })
+
+    except:
+        abort(422)
 
 
 '''
@@ -62,6 +119,34 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/drinks/<int:id>', methods=["PATCH"])
+@requires_auth('patch:drinks')
+def patch_drink(jwt, id):
+
+    drink = Drink.query.get(id)
+
+    #print(drink)
+    body = request.get_json()
+    title = body.get('title')
+    recipe = body.get('recipe')
+
+    if not drink:
+        abort(404)
+
+    try:
+        drink.title = title
+        drink.recipe = json.dumps(recipe)
+        drink.update()
+
+        return jsonify({
+            'success': True,
+            'drinks': drink.long()
+        })
+    except:
+        abort(422)
+
+    
+
 
 '''
 @TODO implement endpoint
@@ -73,6 +158,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(jwt, id):
+
+    drink = Drink.query.get(id)
+
+    if not drink:
+        abort(404)
+
+    try:
+        drink.delete()
+
+        return jsonify({
+            'success': True, 'delete': id
+        })
+    except:
+        abort(422)
 
 
 ## Error Handling
@@ -98,6 +201,14 @@ def unprocessable(error):
 
 '''
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above 
@@ -108,3 +219,11 @@ def unprocessable(error):
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error
+    }), error.status_code
